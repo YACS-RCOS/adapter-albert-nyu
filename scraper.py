9ba618f8ea8784ac1d047644779ad99627b74220
Richie <richiejoeyoung@gmail.com>
2018-04-15 12:58:17 -0400

    add some code to try to handle subjects

--- scraper.py
+++ scraper.py
@@ -15,9 +15,9 @@ def parse():
   driver = webdriver.PhantomJS()
   wait = WebDriverWait(driver, timeout=10)
   driver.get(url)
-  #time.sleep(1)
+  time.sleep(1)
 # Parse the page source
-  soup = BeautifulSoup(driver.page_source, 'html.parser')
+  soup = BeautifulSoup(driver.page_source )
   parseColleges(soup)
   driver.quit()
 
@@ -26,10 +26,20 @@ def parseColleges(soup):
   print(soup.find('div', {'id': 'win0divNYU_CLS_GRP_VW_DESCR254GP$0'}))
   # Get all colleges by id using regular expression
   colleges = soup.findAll('div', id=re.compile('^win0divNYU_CLS_GRP_VW_DESCR254GP\$\d+' ))
-  print(colleges)
-  # Print names of all colleges
+  # print(colleges)
+  # Print names and HTML of all colleges
   for college in colleges:
     print(college.text)
+    print(college)
+    parseSubjectsFromCollege(college)
 
+def parseSubjectsFromCollege(collegesSoup):
+  # This is currently broken. It looks like each college needs to be clicked to expand.
+  # To do this, you may have to pass in the webdriver object, click the <a> element in
+  # each college <div> element, and then create a new soup object from the new HTML.
+  subjects = collegesSoup.findAll('a', id=re.compile('^LINK1\$\d+'))
+  print(subjects)
+  for subject in subjects:
+    print(subject.text)
 
 parse()
